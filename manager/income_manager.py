d0bc4b0b8b8daac8c873707e66ddbd0d0d2e0a68
Maayan Levy <maayan@Maayans-MacBook-Air.local>
2026-01-23 20:52:28 +0200

    Added monthly income sum function

--- manager/income_manager.py
+++ manager/income_manager.py
@@ -1,5 +1,7 @@
 import csv
 
+from datetime import datetime
+
 from consts import *
 
 """
@@ -7,16 +9,41 @@ Income Manager Module - manages and handles all income actions
 """
 class IncomeManager():
     def __init__(self):
-        header = ['date', 'amount', 'description']
+        self.header = {'id_column': 'id', 'date_column' :'date', 'income_column': 'income amount',
+                  'description_column' : 'description'}
+        self.income_file = INCOME_FILE
+        self.current_id = 0
 
         try:
-            with open(INCOME_FILE, mode='x', newline='') as file:
+            with open(self.income_file, mode='x', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow(header)  # Write the header if the file is newly created
-                print(f"Created new file: {INCOME_FILE}")
+                print(f"Created new file: {self.income_file}")
 
-    @staticmethod
-    def write_income(self, income):
-        with open(INCOME_FILE, 'w', newline='') as file:
+    def write_income(self, income: int, date: datetime, description: str):
+        with open(self.income_file, 'w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(income)
+            writer.writerow([self.current_id, date, income, description])
+
+    def sum_income_of_month(self, month: int):
+        total = 0
+        income_column_name = self.header['income_column']
+        date_column_name = self.header['date_column']
+        with open(self.income_file, newline='', encoding='utf-8') as income_file:
+            # Use DictReader to access columns by name (string)
+            reader = csv.DictReader(income_file)
+            if income_column_name not in reader.fieldnames:
+                print(f"Error: Column '{income_column_name}' not found.")
+                return None
+
+            for row in reader:
+                try:
+                    if row[date_column_name].month == month:
+                        # Convert the value to a float (or int if you only have integers) and add to the total
+                        value = row[income_column_name].replace(',', '')  # Handle potential thousand separators
+                        total += float(value)
+                except ValueError:
+                    # Handle cases where a value might be missing or non-numeric (e.g., 'N/A', empty string)
+                    print(f"Warning: Skipping non-numeric value in row: {row[income_column_name]}")
+                    continue
+        return total
